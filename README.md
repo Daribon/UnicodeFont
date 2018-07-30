96866ed04207eb283928568a0b30863644c2adc1
Daribon <dartarifon@hotmail.com>
2018-07-30 18:11:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# UnicodeFont
+Addon to view chinese, russian, english, japanese, korean and a bunch of other languages. For information on what languages are supported, view the FontLicense file.
+
+Based on UnicodeFont 1.4.1, updated to support latest WenQuanYi Zen Hei (0.9.45) font and addon changed to affect chat only.
+
+Keep in mind this addon addon will only make you able to read the supported languages.
+If you are interesting in writting in chinese, russian etc, download Locale Emulator and run Wow.exe with it.
+https://github.com/xupefei/Locale-Emulator
+
+### Setup
+Rename UnicodeFont-master to UnicodeFont and drag it into your addon folder.
+Start up WoW and view some non latin characters, it should now display correctly in chat and in chat bubbles.
+![Image](https://i.imgur.com/CQJIZdF.png)
---
+++ UnicodeFont.lua
@@ -0,0 +1,31 @@
+-- version: 1.4.1b
+-- code inspired by CLEARFONT BY KIRKBURN
+
+UnicodeFontFrame = CreateFrame("Frame", "UnicodeFontFrame")
+
+UNICODEFONT = "Interface\\AddOns\\UnicodeFont\\wqy-zenhei.ttf"
+
+-- Font scale - e.g. if you want all fonts at 80% scale, change '1' to '0.8'
+local SCALE = 1
+
+local function CanSetFont(object) 
+	return (type(object)=="table" and object.SetFont and object.IsObjectType and not object:IsObjectType("SimpleHTML"))
+end
+
+function UnicodeFontFrame:ApplySystemFonts()
+	-- Chat bubbles
+	NAMEPLATE_FONT = UNICODEFONT;
+	-- chat font
+	if (CanSetFont(ChatFontNormal)) then ChatFontNormal:SetFont(UNICODEFONT, 14 * SCALE); end
+end
+
+UnicodeFontFrame:SetScript("OnEvent",
+	function() 
+		if (event == "ADDON_LOADED") then
+			UnicodeFontFrame:ApplySystemFonts()
+		end
+	end);
+	
+UnicodeFontFrame:RegisterEvent("ADDON_LOADED");
+
+UnicodeFontFrame:ApplySystemFonts()
